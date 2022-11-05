aa3347d45a31f660e725202a990c87721111c5d1
Imperium <piramida.and.tochka@gmail.com>
2022-11-05 22:25:00 +0500

    Commit #1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import {useEffect} from 'react';
-const tg = window.Telegram.WebApp;
+import {useTelegram} from './hooks/useTelegram';
 
 function App() {
-
+    const {onToggleButton, tg} = useTelegram()
     useEffect(() => {
            tg.ready();
     }, []);
@@ -11,8 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      Начать
-        <button onClick={onClose}>Закрыть</button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
